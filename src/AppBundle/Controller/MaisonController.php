0cce10207697f39fbe9028474ed8c3b6dfb3733e
Jérémie D <jeremie.de20@gmail.com>
2017-03-09 09:56:16 +0100

    13

---
+++ src/AppBundle/Controller/MaisonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/maisons", name="listerMaison")
+     */
+    public function indexAction()
+    {
+        // récupérer l'entity manager : la connexion à la BDD
+        $em=$this->getDoctrine()->getManager();
+        $maisons=$em->getRepository("AppBundle:Maison")->findAll();
+        // replace this example code with whatever you need
+        return $this->render('Maison/index.html.twig',["maisons"=>$maisons]);
+    }
+}
