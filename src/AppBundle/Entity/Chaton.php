00551bfa5fa6e61d6d94a98f9be2214ad3104e96
Jérémie D <jeremie.de20@gmail.com>
2017-03-09 08:47:00 +0100

    2
    2eme commit

---
+++ src/AppBundle/Entity/Chaton.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Chaton
+ *
+ * @ORM\Table(name="chaton")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ChatonRepository")
+ */
+class Chaton
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateNaissance", type="date", nullable=true)
+     */
+    private $dateNaissance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="race", type="string", length=100, nullable=true)
+     */
+    private $race;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="urlPhoto", type="string", length=255)
+     */
+    private $urlPhoto;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Chaton
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set dateNaissance
+     *
+     * @param \DateTime $dateNaissance
+     *
+     * @return Chaton
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    /**
+     * Get dateNaissance
+     *
+     * @return \DateTime
+     */
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * Set race
+     *
+     * @param string $race
+     *
+     * @return Chaton
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * Get race
+     *
+     * @return string
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * Set urlPhoto
+     *
+     * @param string $urlPhoto
+     *
+     * @return Chaton
+     */
+    public function setUrlPhoto($urlPhoto)
+    {
+        $this->urlPhoto = $urlPhoto;
+
+        return $this;
+    }
+
+    /**
+     * Get urlPhoto
+     *
+     * @return string
+     */
+    public function getUrlPhoto()
+    {
+        return $this->urlPhoto;
+    }
+}
+
